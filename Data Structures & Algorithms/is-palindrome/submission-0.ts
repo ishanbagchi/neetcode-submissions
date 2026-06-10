ac8095e8fba845abcb689236c644b15fa533d8c7
Ishan Bagchi <ishanbagchi4@gmail.com>
2026-06-10 19:03:53 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.ts
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        const cleaned = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+        let left = 0,
+            right = cleaned.length - 1;
+        while (left < right) {
+            if (cleaned[left++] !== cleaned[right--]) return false;
+        }
+        return true;
+    }
+}
