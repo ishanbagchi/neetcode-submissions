b4edb13d63d8e25663c8107b41230959f727f58f
Ishan Bagchi <ishanbagchi4@gmail.com>
2026-06-13 17:33:01 +0530

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.ts
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs: string[]): string {
+        return strs.map(str => `${str.length}#${str}`).join('')
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str: string): string[] {
+        const result = []
+        let i = 0
+
+        while(i < str.length) {
+            let j = i
+            while (str[j] !== '#') j++
+            const len = parseInt(str.slice(i, j))
+            result.push(str.slice(j + 1, j + 1 + len))
+            i = j + len + 1
+        }
+
+        return result
+    }
+}
